6cbd1e64e8082a2c26dff9d4c3a78e598944997c
TomasBacchetta <78171375+TomasBacchetta@users.noreply.github.com>
2022-08-26 19:48:32 -0300

    aa

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BienvenidoComponent } from './Vistas/bienvenido/bienvenido.component';
+import { LoginComponent } from './Vistas/login/login.component';
+import { ErrorComponent } from './Vistas/error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BienvenidoComponent,
+    LoginComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
